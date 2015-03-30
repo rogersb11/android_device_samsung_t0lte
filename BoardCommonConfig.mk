11698eeda59f48620ae96b2d7b7e1888bd34f092
Nathan Prevette <nprevette420@gmail.com>
2015-03-29 21:26:22 -0700

    Fix build error

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -32,7 +32,9 @@ COMMON_GLOBAL_CFLAGS += -DCAMERA_WITH_CITYID_PARAM
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 ifeq ($(TARGET_VOICE_TECH), cdma)
-TARGET_KERNEL_CONFIG := custom_t0lte_defconfig
+TARGET_KERNEL_CONFIG := liquid_t0ltecdma_defconfig
+else
+TARGET_KERNEL_CONFIG := liquid_t0lte_defconfig
 endif
 
 # Recovery
